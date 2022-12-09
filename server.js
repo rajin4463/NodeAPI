b7f04dfb982c4252298f6816aaabbe64cf7571d8
rajin4463 <73782473+rajin4463@users.noreply.github.com>
2022-12-09 16:46:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+console.log('Server is starting');
+
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+const Server = app.listen(3000, listening);
+
+
+function listening(){
+    console.log('listening.....');
+}
+
+app.use(express.static('website'));
+
+
+app.get('/name', sendName)
+
+function sendName(request, response){
+    response.send('Rajin');
+}
